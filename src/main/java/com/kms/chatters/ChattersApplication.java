7965c2e432a46366e5f74266ab0930e2e9c54a39
kms <kmmin78@naver.com>
2020-08-11 00:09:07 +0900

    2020-08-11 12:08  - first commit

---
+++ src/main/java/com/kms/chatters/ChattersApplication.java
@@ -0,0 +1,13 @@
+package com.kms.chatters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChattersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kms/chatters/ChattersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kms.chatters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
